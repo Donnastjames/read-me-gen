98bfbc6c1a0ea97695f0d7f0fee84694aa9b94d8
Donna <donnastjames@comcast.net>
2021-05-19 18:22:32 -0700

    Added question prompting user for the output file name

--- index.js
+++ index.js
@@ -52,15 +52,21 @@ const questions = [
     message: 'Please select a license:',
     choices: ['Apache 2.0 License', 'Boost Software License 1.0', 'BSD 3-Clause License', 'BSD 2-Clause License', 'CC0', 'Attribution 4.0 International', 'Attribution-ShareAlike 4.0 International', 'Attribution-NonCommercial 4.0 International', 'Attribution-NoDerivates 4.0 International', 'Attribution-NonCommmercial-ShareAlike 4.0 International', 'Attribution-NonCommercial-NoDerivatives 4.0 International', 'Eclipse Public License 1.0', 'GNU GPL v3', 'GNU GPL v2', 'GNU AGPL v3', 'GNU LGPL v3', 'GNU FDL v1.3', 'IBM Public License Version 1.0', 'ISC License (ISC)', 'The MIT License', 'Mozilla Public License 2.0', 'Attribution License (BY)', 'Open Database License (ODbL)', 'Public Domain Dedication and License (PDDL)', 'The Perl License', 'The Artistic License 2.0', 'SIL Open Font License 1.1', 'The Unlicense', 'The Do What the F**k You Want to Public License', 'The zlib/libpng License'],
   },
+  // Added question to prompt user for the output file ...
+  {
+    type: 'input',
+    name: 'fileName',
+    message: 'Please type the file name you would like to save to:',
+    default: 'README.md'
+  },
 ];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, err =>
-      err ? console.error(err) : console.log(`Successfully created ${fileName}!`)
+      err ? console.error(err) : console.log(`Successfully created "${fileName}"`)
     );
 }
-// const writeToFile = util.promisify(fs.writeFile);
 
 const promptUser = () => {
   return inquirer.prompt(questions);
@@ -69,7 +75,7 @@ const promptUser = () => {
 // Create a function to initialize app
 const init = () => {
   promptUser()
-    .then(data => writeToFile('README2.md', generateMarkdown(data)))
+    .then(data => writeToFile(data.fileName, generateMarkdown(data)))
     .catch((err) => console.error(err));
 };
 
